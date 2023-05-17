8128d793d788a532509165a0684055c0eb82043b
Wycliffe <wycliffekiprono4@gmail.com>
2023-05-17 15:43:31 +0300

    done

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,10 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+require 'pry'
+
+def parrot (sound = "Squawk!")
+   puts sound 
+   sound
+end 
+
+parrot ("Whistle")
